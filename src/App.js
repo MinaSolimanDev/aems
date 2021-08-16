66974249e2e08cbc96f6450c11ddadc178c652a9
hassan.mokhtar296 <hassan.mokhtar296@gmail.com>
2021-08-16 14:35:45 +0200

    parallel

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Parallel from './components/parallelDiagram/Parallel';
+import './App.css';
+
+function App() {
+  return (<div>
+    <Parallel />
+  </div>
+  );
+}
+
+export default App;
